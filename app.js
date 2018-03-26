179b1e8ca723768766b1fec9a620a6ad0a112651
julianlee98 <julian_lee_@outlook.com>
2018-03-26 14:22:26 +1100

    stuff for heroku

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.set('view engine', 'ejs');
+
+app.listen(PORT, function(){
+   console.log("Express listening on port ${PORT}");
+});
 
 const router = require('./routes/routes');
 
