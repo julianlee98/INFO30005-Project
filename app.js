dbefa55c98d92660a6cb3484cd8bf0ba9c5e8c55
Julian Lee <julian_lee_@outlook.com>
2018-03-26 16:55:12 +1100

    jquery added 2

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(router);
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.listen(PORT, function(){
     console.log("Express listening on port ${PORT}");
--- applocal.js
+++ applocal.js
@@ -8,6 +8,7 @@ const router = require('./routes/routes');
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use(router);
  app.listen(3000, function(){
