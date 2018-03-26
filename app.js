fd02c729265bc4ad535d922ad325c14e7bface2a
julianlee98 <julian_lee_@outlook.com>
2018-03-26 14:24:26 +1100

    stuff for heroku 2

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 
-app.listen(PORT, function(){
-   console.log("Express listening on port ${PORT}");
-});
-
 const router = require('./routes/routes');
 
 app.use(router);
 
-app.listen(3000, function(){
-    console.log("Listening");
+app.listen(PORT, function(){
+    console.log("Express listening on port ${PORT}");
 });
+
+// app.listen(3000, function(){
+//     console.log("Listening");
+// });
 
