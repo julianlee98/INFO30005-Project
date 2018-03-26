2f4a48a7b3293cc7221ac79e3dbf8a3e29383d4d
Julian Lee <julian_lee_@outlook.com>
2018-03-26 16:32:15 +1100

    bootstrap implemented

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const router = require('./routes/routes');
 
 app.use(router);
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.listen(PORT, function(){
     console.log("Express listening on port ${PORT}");
 });
--- applocal.js
+++ applocal.js
@@ -5,6 +5,10 @@ app.set('view engine', 'ejs');
 
 const router = require('./routes/routes');
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
 app.use(router);
  app.listen(3000, function(){
      console.log("Listening");
