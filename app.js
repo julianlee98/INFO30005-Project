a327165940655854f804310863e6694cab705111
julianlee98 <julian_lee_@outlook.com>
2018-03-27 15:27:16 +1100

    Coming soon page done

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); /
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/myCss', express.static(__dirname + '/views/Css/'));
 
 app.listen(PORT, function(){
     console.log("Express listening on port ${PORT}");
