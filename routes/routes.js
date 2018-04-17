6c0316afd60e05094c71206f9aec222afd6d2605
julianlee98 <julian_lee_@outlook.com>
2018-04-18 00:22:15 +1000

    fixes

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controller = require("../controllers/testDb Controller");
 
+router.get('/', controller.loginPage);
 router.get('/login', controller.loginPage);
 router.get('/home', controller.homePage);
 router.get('/settings', controller.settingsPage);
